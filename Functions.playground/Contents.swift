e639b6d27e0a74f16ca3b1db3556a23e5b2bf5c6
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-07-05 18:28:01 -0400

    Done.

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,6 +1,7 @@
 func sayHello() {
     let greeting = "Hello there!"
-    print(greeting)
+    print(greeting)   //  Every function has its own scope, which means that 
+    //constants and variables defined in that function are only usable within that function.
 }
 
 sayHello()
@@ -8,8 +9,8 @@ sayHello()
 
 // Uncomment this line to see the error that is printed in the console.
 // (You can uncomment a line by removing the // at the beginning of the line.)
-//print(greeting)
 
+//print(greeting)  // uncommented should get an error because it it outside of the function
 
 func sayHelloToGarfield() {
     print("Hello, Garfield, why do you sleep so much?")
@@ -24,14 +25,17 @@ func sayHelloToScratchy() {
 }
 
 
-func sayHelloToCat(catName: String) {
-    print("Hello, \(catName), why do you sleep so much?")
+
+func sayHelloToCat(catName: String) {  //  declared an argument called catName of type String
+    print("Hello, \(catName), why do you sleep so much?")  // 'catName' is visible only within the scope
+    //of sayHelloToCat, so you cannot refer to it outside of the function.
 }
 
-sayHelloToCat("Mittens")
+sayHelloToCat("Mittens")  // calling the function setting the value to the catName
 
 let catFriend = "Socks"
-sayHelloToCat(catFriend)
+sayHelloToCat(catFriend) // You can call sayHelloToCat() just like you called sayHello(), 
+//  except you have to pass an argument to sayHelloToCat()
 
 // Uncomment this line to see the error that is printed in the console.
-//sayHelloToCat()
+sayHelloToCat("Beauty")
